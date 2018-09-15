018e0d0cb56e62b79e485b5da1da5f2c5731bc9e
newsour <42475390+newsour@users.noreply.github.com>
2018-09-15 22:44:47 +0900

    layout調整 APIコールタイミングの変更

--- app/src/main/java/com/example/tatsuro/sportable/ShopsFragment.kt
+++ app/src/main/java/com/example/tatsuro/sportable/ShopsFragment.kt
@@ -23,7 +23,6 @@ class ShopsFragment : Fragment() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val args = arguments
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.shops_fragment, container, false)
